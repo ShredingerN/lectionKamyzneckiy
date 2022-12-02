6f36334012de8b4103c6a3ccaa16eac912ca8c16
Natalia <nataxawolf@gmail.com>
2022-12-02 16:24:28 +0300

    folders

---
+++ Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+int count = 0;
+int first_friend_speed = 1;
+int second_friend_speed = 2;
+int dog_speed = 5;
+int friend = 2;
+int distance = 10000;
+
+while (distance < 10000)
+{
+    if (firend == 1)
+    {
+        time = distance/(first_friend_speed + dog_speed);
+        friend = 2;
+    }
+    else
+    {
+       time = distance/(second_friend_speed + dog_speed);
+       friend = 1;
+    }
+    distance = distance - (first_friend_speed + second_friend_speed) * time;
+    count++;
+}
+Console.WriteLine(count);
---
+++ L2Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Нахождение максимума из 9 чисел
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 11;
+int c1 = 34;
+int a2 = 6;
+int b2 = 9;
+int c2 = 234;
+int a3 = 27;
+int b3 = 79;
+int c3 = 56;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
